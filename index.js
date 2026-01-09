7c30f5f7c6562923dfc9db15d4e0e3fe5f01e58d
Vandanalodhi2005 <vandanalodhi2005@gmail.com>
2026-01-09 16:33:20 +0530

    git ignore

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/",(req,res)=> {
+  res.send("vandana")
+})
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/register", registrationRoutes);
